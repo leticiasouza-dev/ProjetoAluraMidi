5d3fd62f0e0048c72b160676598fa703a1440dea
LetíciaSouza <119368474+leticiasouza-dev@users.noreply.github.com>
2023-06-13 19:54:45 -0300

    removendo variáveis indesejadas

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+    document.querySelector(idElementoAudio).play(); /*reproduzir som */
 }
 /* Com o *onclick* quando eu clicar no botão vai tocar o som que esta na funcão (tocaSomPom)
 document.querySelector('.tecla_pom').onclick = tocaSomPom;*/
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
 let contador = 0
+
 //enquanto
 while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
+   
     
-    console.log(instrumento);
-
-    tecla.onclick = function() {
+    listaDeTeclas[contador].onclick = function() {
         tocaSom('#som_tecla_pom');
     }
 
     contador = contador + 1;
+
     console.log(contador);
 }
 
